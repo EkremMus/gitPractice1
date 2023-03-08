c54efc4ac293a0b530e255eaedbe45ca855a95b2
EkremMus <102433037+EkremMus@users.noreply.github.com>
2023-03-08 08:07:21 -0600

    Update HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
 // everything is going out of my hands
 public static void main(String[] args) {
 
-    // it is driving a new Maserati down to dead-end street
+    // it is like driving a new Maserati down to dead-end street
 
     // turning red
 
